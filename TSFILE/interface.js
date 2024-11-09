863ee5b04c62b7a123dd8ee8c5a663ea7a6c3c74
maliksartaj <153930662+maliksartaj@users.noreply.github.com>
2024-11-09 19:43:50 +0530

    Delete TSFILE directory

--- TSFILE/interface.js
+++
@@ -1,9 +0,0 @@
-var area_s = function (d) {
-    var side = d.length = d.width;
-    console.log("Area::".concat(side * side));
-};
-area_s({ length: 10, width: 20 });
-var area_r = function (d) {
-    console.log("Area::".concat(d.length * d.width));
-};
-area_r({ length: 10, width: 20 });
--- TSFILE/prac1.js
+++
@@ -1,5 +0,0 @@
-var dist = function (pt1, pt2) {
-    console.log("horizontal distance::{pt1.x1-pt2.x2}");
-    console.log("vertical distance::{pt1.y1-pt2.y2}");
-};
-dist({ x1: 10, y1: 20 }, { x2: 10, y2: 20 });
